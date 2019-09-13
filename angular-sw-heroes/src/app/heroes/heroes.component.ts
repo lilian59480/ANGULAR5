9e445440c43a0fb86b460fc2951530098bcd83af
Lilian Petitpas <lilian.petitpas@outlook.com>
2019-09-13 09:17:53 +0200

    [APP] Hero list finished

--- angular-sw-heroes/src/app/heroes/heroes.component.ts
+++ angular-sw-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../models/hero.model';
-
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'sw-heroes',
@@ -9,14 +9,18 @@ import { Hero } from '../models/hero.model';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: "Jar Jar Binks"
-  }
+  heroes: Hero[] = HEROES
+
+  selectedHero: Hero
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero) {
+    console.log(hero)
+    this.selectedHero = hero;
   }
 
 }
